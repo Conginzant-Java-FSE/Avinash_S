3010cb10485744883eb3030d3bfdbae3d23904fd
Avinash <166938611+Avinash0527@users.noreply.github.com>
2026-02-18 13:06:42 +0530

    Create readonly.ts

---
+++ TSadvanced/readonly.ts
@@ -0,0 +1,10 @@
+function sortLeaderboard<T extends { score: number }>(
+  players: readonly T[]
+): T[] {
+  return [...players].sort((a, b) => b.score - a.score);
+}
+const leaderboard = [
+  { name: "Alex", score: 120 },
+  { name: "Mira", score: 200 }
+] as const;
+console.log(sortLeaderboard(leaderboard));
