75b50a4489785b3f861e6d54d1d5f84d318594e1
Avinash <166938611+Avinash0527@users.noreply.github.com>
2026-02-18 13:05:52 +0530

    Create omitutility.ts

---
+++ TSadvanced/omitutility.ts
@@ -0,0 +1,18 @@
+interface Account {
+  id: number;
+  email: string;
+  password: string;
+}
+function sanitizeAccount(
+  account: Account
+): Omit<Account, "password"> {
+  const { password, ...rest } = account;
+  return rest;
+}
+console.log(
+  sanitizeAccount({
+    id: 1,
+    email: "user@app.com",
+    password: "secret123"
+  })
+);
